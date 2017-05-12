cd449f9c5e6c222fa50e56fab826a8517a8ee2b0
thi-bow <ground_thi_bo@yahoo.co.jp>
2017-05-12 15:00:05 +0900

    タイトル画面の作成

---
+++ Assets/Kageyama/Script/ButtonFlash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonFlash : MonoBehaviour
+{
+    private RectTransform _myRect;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _myRect = this.GetComponent<RectTransform>();
+        LeanTween.scale(_myRect, new Vector2(1.0f, 1.0f), 2)
+            .setLoopPingPong()
+            .setEase(LeanTweenType.easeOutQuart);
+        LeanTween.alpha(_myRect, 1.0f, 2)
+            .setLoopPingPong()
+            .setEase(LeanTweenType.easeOutQuart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
