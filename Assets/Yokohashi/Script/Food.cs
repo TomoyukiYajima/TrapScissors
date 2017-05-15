bf9b3280d7e7bba0216478e445d2bae93f6e1dc2
mineralwater2000 <4584.ron@gmail.com>
2017-05-15 16:55:20 +0900

    餌の画像切り替えの追加

---
+++ Assets/Yokohashi/Script/Food.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour {
+
+    public enum Food_Kind
+    {
+        NULL,
+        Goat,
+        Tanuki,
+        Rabbit
+    }
+
+    public Sprite[] _sprite;
+
+    public Food_Kind food_Kind;
+
+    // Use this for initialization
+    void Start () {
+
+        food_Kind = Food_Kind.NULL;
+    }
+	
+	// Update is called once per frame
+	void Update () {    
+	    
+	}
+
+    public void SelectFood(int food)
+    {
+        if (food == 3) 
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else if(food == 0)
+        {
+            food_Kind = Food_Kind.Goat;
+        }
+        else if (food == 1)
+        {
+            food_Kind = Food_Kind.Tanuki;
+        }
+        else if (food == 2)
+        {
+            food_Kind = Food_Kind.Rabbit;
+        }
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = _sprite[food];
+    }
+    public Food_Kind CheckFoodKind()
+    {
+        return food_Kind;
+    }
+}
