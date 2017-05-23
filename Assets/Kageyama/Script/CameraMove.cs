c279504a0d9476a5e3dacfd6fb3e9545dbd43e34
thi-bow <ground_thi_bo@yahoo.co.jp>
2017-05-23 16:29:34 +0900

    ロック中のカメラの移動

--- Assets/Kageyama/Script/CameraMove.cs
+++ Assets/Kageyama/Script/CameraMove.cs
@@ -10,24 +10,69 @@ public class CameraMove : MonoBehaviour
     [SerializeField, TooltipAttribute("追尾させるかどうか")]
     private bool _moveFrag;
 
+    //プレイヤーの移動を固定して、カメラで全体を見渡せるようにする
+    [SerializeField]
+    private bool _playerMoveLock;
+    private Vector3 _playerReturnPos;
+    [SerializeField, TooltipAttribute("カメラの移動速度")]
+    private float _speed;
+    private Vector3 _moveDirection = Vector3.zero;
+    private float _clampX;
+    private float _clampZ;
+    private Vector3 newPosition;
+
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         _offset = transform.position - _player.transform.position;
+        _clampX = GameManager.gameManager.ClampX();
+        _clampZ = GameManager.gameManager.ClampZ();
+        _playerMoveLock = false;
     }
 
     void LateUpdate()
     {
-        if (_moveFrag == false) return;
+        newPosition = transform.position;
+        //プレイヤーと一緒に動く
+        if (_playerMoveLock == false)
+        {
+            if (_moveFrag == false) return;
 
-        Vector3 newPosition = transform.position;
-        newPosition.x = _player.transform.position.x + _offset.x;
-        newPosition.y = _player.transform.position.y + _offset.y;
-        newPosition.z = _player.transform.position.z + _offset.z;
-        //ピッタリと追いかける
-        if (_lerpFrag == false) transform.position = newPosition;
-        //スムーズに追いかける
-        else transform.position = Vector3.Lerp(transform.position, newPosition, 5.0f * Time.deltaTime);
+            newPosition.x = _player.transform.position.x + _offset.x;
+            newPosition.y = _player.transform.position.y + _offset.y;
+            newPosition.z = _player.transform.position.z + _offset.z;
+            //ピッタリと追いかける
+            if (_lerpFrag == false) transform.position = newPosition;
+            //スムーズに追いかける
+            else transform.position = Vector3.Lerp(transform.position, newPosition, 5.0f * Time.deltaTime);
+        }
+    }
+
+    void Update()
+    {
+        if (_playerMoveLock == true)
+        {
+            newPosition = transform.position;
+            _moveDirection = (Vector3.forward - Vector3.right) * Input.GetAxis("Vertical") + (Vector3.forward + Vector3.right) * Input.GetAxis("Horizontal");
+            newPosition += _moveDirection * Time.deltaTime * _speed;
+            transform.position = new Vector3(Mathf.Clamp(newPosition.x, -100.0f, _clampX),
+                                         newPosition.y,
+                                         Mathf.Clamp(newPosition.z, -30.0f, _clampZ));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (_playerMoveLock == true)
+            {
+                transform.position = _playerReturnPos;
+                _playerMoveLock = false;
+            }
+            else
+            {
+                _playerReturnPos = transform.position;
+                _playerMoveLock = true;
+            }
+        }
     }
 
     /// <summary>
