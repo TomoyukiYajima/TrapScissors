430d97145a26f3de873bfc422c6bd6ceba4a7d3e
Suzuki Ukyo <togingham@gmail.com>
2017-06-27 16:01:52 +0900

    バックログ更新・時間経過の修正（鈴木）

--- Assets/Suzuki/Item/Program/RotateSkybox.cs
+++ Assets/Suzuki/Item/Program/RotateSkybox.cs
@@ -2,25 +2,38 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class RotateSkybox : MonoBehaviour {
+public class RotateSkybox : MonoBehaviour
+{
+    [SerializeField, TooltipAttribute("朝方")]
+    private Material Dawn;
+    [SerializeField, TooltipAttribute("朝")]
+    private Material Morning;
+    [SerializeField, TooltipAttribute("夕焼け")]
+    private Material Evening;
+    [SerializeField, TooltipAttribute("夜")]
+    private Material Night;
+    [SerializeField, TooltipAttribute("曇り")]
+    private Material Cloudy;
     public float _anglePerFrame = 0.1f;    // 1フレームに何度回すか
-    private float _rot = 0.0f;
-    public static Material Skybox;
+    public float _rot = 0f;//0.03fが基本
+    public GameObject _light;
     // Use this for initialization
-    void Start()
-    {
+    void Start(){
+        RenderSettings.skybox = Morning;
     }
-
     // Update is called once per frame
     void Update()
     {
         _rot += _anglePerFrame;
         if (_rot >= 360.0f)
-        {    
-            _rot -= 360.0f;
+        {
+            RenderSettings.skybox = Morning;
+            _rot -= 360.0f;//1周(360°)すると0°になり、再度加算される
         }
-        RenderSettings.skybox.SetFloat("_Rotation", _rot);    //回転する
+        else if (_rot >= 330.0f) RenderSettings.skybox = Dawn;
+        else if (_rot >= 210.0f) RenderSettings.skybox = Night;
+        else if (_rot >= 180.0f) RenderSettings.skybox = Evening;
 
-        
+ RenderSettings.skybox.SetFloat("_Rotation", _rot);    //_rotが °の役割になり回転する
     }
 }
