b37c08bdf554d20204b36668bdc44bd9ecd29452
Suzuki Ukyo <togingham@gmail.com>
2017-07-11 16:03:59 +0900

    バックログ更新（鈴木）・リファクタリング・オーバ画面UIの修正

--- Assets/Suzuki/Item/Program/BigTrap.cs
+++ Assets/Suzuki/Item/Program/BigTrap.cs
@@ -6,18 +6,18 @@ public class BigTrap : MonoBehaviour
 {
     public enum TrapState
     {
-        WAIT,       //動物が掛かるのを待っている状態
-        CAPTURE,    //動物を捕まえている状態
+        WAIT,       //待機状態
+        CAPTURE,    //捕獲状態
     }
     public TrapState _state;
     public bool _istutorial = false;
-    public int _stagenum=0;
-    public GameObject _traphit;
+    public int _stagenum = 0;
+    public GameObject _traphit;                  //エフェクトの表示
     [SerializeField, TooltipAttribute("Resultで表示されるオブジェクト")]
     private GameObject _result;
-    [SerializeField]
+    [SerializeField, TooltipAttribute("捕獲時の動物")]
     private GameObject _targetAnimal;
-    private bool _flg;
+    private bool _flg;                           //捕獲状態事に判定
     private Animator _animator;
 
     void Start()
@@ -33,22 +33,22 @@ public class BigTrap : MonoBehaviour
     {
         if (col.tag == "LargeEnemy")
         {
-            SoundManger.Instance.PlaySE(10);
             Enemy3D animal = col.gameObject.GetComponent<Enemy3D>();
             animal.ChangeTrap(gameObject);
             ChengeState(TrapState.CAPTURE);
             _targetAnimal = col.gameObject;
+            _animator.SetBool("Close", true); _flg = true;
+            SoundManger.Instance.PlaySE(10);
             Instantiate(_traphit, this.transform.position, Quaternion.identity);
-            _animator.SetBool("Close", true);
-            _flg = true;
-            _animator.CrossFade("Hit", 0.1f, -1);
+            transform.localScale = new Vector3(10,10,10); //縮小対策
+            _animator.CrossFade("Hit", 0.1f, -1, 0);
             GameManager.gameManager.HuntCountAdd();
             if (_istutorial == false)
             {
                 _result.SetActive(true);
                 GameManager.gameManager.GameStateSet(GameManager.GameState.END);
             }
-            if (_stagenum == null)
+            if (_stagenum == null) //次ステージへ移行できるようにする
             {
                 GameObject _openstage = GameObject.Find("OpenStage");
                 _openstage.GetComponent<OpenStage>().StageSet(_stagenum);
